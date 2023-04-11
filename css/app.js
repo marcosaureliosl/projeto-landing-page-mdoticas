f7601192363b47b87f7d802eac18f75fe3aab528
Marcos Aurelio <127764997+marcosaureliosl@users.noreply.github.com>
2023-04-11 13:28:36 -0300

    Add files via upload

---
+++ css/app.js
@@ -0,0 +1,14 @@
+function abrirmenu() {
+    let BarraMenuAberto = document.getElementById ('Barra-menu')
+
+    if (BarraMenuAberto.style.width == "0px") {
+        BarraMenuAberto.style.width = "300px"
+    }
+    else{
+        BarraMenuAberto.style.width = "0px"
+    }
+    
+
+   
+    
+}
